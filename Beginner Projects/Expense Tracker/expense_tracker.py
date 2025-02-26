da9b142dd6f4092a02bfefa7c0dd4a59962d0711
Naga <naga.sudeep@gmail.com>
2025-02-26 15:17:19 +0530

    added the main function for cli arguments

--- Beginner Projects/Expense Tracker/expense_tracker.py
+++ Beginner Projects/Expense Tracker/expense_tracker.py
@@ -26,3 +26,21 @@ def add_expense(description,amount):
     conn.close()
 
     print(f"✅ Expense added successfully (ID: {expense_id})")
+
+#Main function to handle CLI commands
+
+def main():
+    parser = argparse.ArgumentParser(description="Expense Tracker CLI")
+
+    #Add command
+    parser.add_argument("add", help="Add a new expense", nargs='?',default=None)
+    parser.add_argument("--description",required=True,help="Description of the expense")
+    parser.add_argument("--amount",type=float,required=True,help="Amount of the expense")
+
+    args = parser.parse_args()
+
+    if args.add == "add":
+        add_expense(args.description,args.amount)
+
+if __name__ == "__main__":
+    main()
