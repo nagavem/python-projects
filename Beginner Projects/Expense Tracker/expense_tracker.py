9e20ba18b656d742c4b69c261203da197a3378ef
Naga <naga.sudeep@gmail.com>
2025-02-27 15:50:59 +0530

    added update function

--- Beginner Projects/Expense Tracker/expense_tracker.py
+++ Beginner Projects/Expense Tracker/expense_tracker.py
@@ -29,6 +29,48 @@ def add_expense(description,amount):
 
     print(f"✅ Expense added successfully (ID: {expense_id})")
 
+#Fn to update an expense using the id
+def update_expense(expense_id, description = None, amount = None):
+    conn=sqlite3.connect("expenses.db")
+    cursor = conn.cursor()
+
+    #check if expense exists
+    cursor.execute("SELECT * FROM expenses where id = ?", (expense_id,))
+    expense = cursor.fetchone
+    if not expense:
+        print(f"❌ Expense with ID {expense_id} not found.")
+        conn.close()
+        return
+    
+    #prepare the update query
+    updates = []
+    values = []
+
+    if description:
+        updates.append("description = ?")
+        values.append(description)
+    
+    if amount:
+        updates.append("amount = ?")
+        values.append("amount")
+
+    if not updates:
+        print("⚠ No changes provided.")
+        conn.close()
+        return
+    
+    values.append(expense_id)
+    query = f"UPDATE expenses SET {', '.join(updates)} WHERE id = ?"
+
+    # Execute the update query
+    cursor.execute(query,values)
+    conn.commit()
+    conn.close()
+
+    print(f"✅ Expense (ID: {expense_id}) updated successfully!")
+
+
+
 #Main function to handle CLI commands
 
 def main():
